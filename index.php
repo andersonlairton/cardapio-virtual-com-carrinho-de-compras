784cd6dc2fda5919ac0d100f31bc606b4b40d1ae
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-08 00:31:16 -0300

    acessando todos os dados do json via foreach

--- index.php
+++ index.php
@@ -6,11 +6,32 @@
     $json_str = json_decode($json_file, true);
     $itens = $json_str['grupos'];
 
-    foreach ( $itens as $e ) { 
-        echo $e['cdGrupo']."<br>";
-        echo $e['dsGrupo']."<br>";
+    foreach ( $itens as $e=>$value ) {
+       // var_dump($e); 
+        echo $value['cdGrupo']."<br>";
+        echo $value['dsGrupo']."<br>";//descrição do grupo
+       
+        //varivel que pega os produtos
         //echo $e['produtos']."<br>";
-        var_dump(is_array($e['produtos']));
+        //var_dump(is_array($value['produtos']));//produtos pertencentes ao grupo
+        foreach($value['produtos'] as $p=>$prod){//acessando os produtos de cada grupo
+            //var_dump(is_array($prod['adicionais']));
+            echo $prod['cdProduto']."<br>";
+            echo $prod['dsObservacao']."<br>";
+            echo $prod['dsProduto']."<br>";
+            echo $prod['vlProduto']."<br>";
+            foreach($prod['adicionais'] as $a=>$adic){
+                echo $adic['cdAdicional']."<br>";
+                echo $adic['dsAdicional']."<br>";
+                echo $adic['vlAdicional']."<br>";
+            }
+            //echo $prod['cdProduto']."<br>";
+        }
+        /*
+        var_dump(is_array($e['Complemento']));
+        var_dump(is_array($e['Bebidas']));
+        var_dump(is_array($e['Diversos']));
         //echo $e['produtos']['Complemento']."<br>";
+        */
     }
 
